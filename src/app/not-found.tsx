d58727e5f72f66089973f943386cd70d9ab2f6b6
Oleksandr Dzisiak <56607007+SanekxArcs@users.noreply.github.com>
2025-03-03 16:06:18 +0100

    Remove back button from NotFound component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -21,12 +21,6 @@ export default function NotFound() {
         >
           Strona główna
         </Link>
-        <button
-          onClick={() => window.history.back()}
-          className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 py-2 px-4"
-        >
-          Wróć do poprzedniej strony
-        </button>
       </div>
     </div>
   );
